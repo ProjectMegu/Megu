f132758b425acc1e5db0deef8849e83c7ad0ddf4
sakura_aoi <souyakoji@gmail.com>
2024-02-18 03:31:41 +0000

    add wasm tools & aidan binding
    tools:
    * wabt
    * binaryen

---
+++ libs/aidan-rs/src/module.rs
@@ -0,0 +1,39 @@
+use binaryen_capi_sys as bapi;
+
+mod feature;
+
+pub struct Module {
+    ref_: bapi::BinaryenModuleRef
+}
+
+impl Default for Module {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Drop for Module {
+    fn drop(&mut self) {
+        unsafe { bapi::BinaryenModuleDispose(self.ref_) }
+    }
+}
+
+impl Module {
+    pub fn new() -> Module {
+        Module {
+            ref_: unsafe { bapi::BinaryenModuleCreate() }
+        }
+    }
+
+    pub fn print(&self) {
+        unsafe { bapi::BinaryenModulePrint(self.ref_) }
+    }
+
+    pub fn set_features(&self, feature: &[feature::Feature]) {
+        let mut features: bapi::BinaryenFeatures = 0;
+        for f in feature {
+            features |= f.return_feature();
+        }
+        unsafe { bapi::BinaryenModuleSetFeatures(self.ref_, features) }
+    }
+}
