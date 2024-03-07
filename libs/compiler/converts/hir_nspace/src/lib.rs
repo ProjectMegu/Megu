935aa27e19a1fb3cef9a03cd13f8eaaa300ccfea
sakura_aoi <souyakoji@gmail.com>
2024-03-07 18:47:36 +0900

    ??

--- libs/compiler/converts/hir_nspace/src/lib.rs
+++ libs/compiler/converts/hir_nspace/src/lib.rs
@@ -1,6 +1,7 @@
 use hir::HirCtx;
 
 pub fn pass_nspace(mut ctx: HirCtx) -> HirCtx {
+    // Change NameSpace
     for module in &mut ctx.mods {
         let items = module.items.clone();
         for (nspace, item) in items {
@@ -17,6 +18,30 @@ pub fn pass_nspace(mut ctx: HirCtx) -> HirCtx {
                         let mut name = data.line_nspace.name.clone();
                         name.push(nspace[1].clone());
                         module.items.insert(name, item);
+                    }
+                }
+            }
+        }
+    }
+
+    // Change Use
+    for module in &mut ctx.mods {
+        for item in module.file_item.values_mut() {
+            for use_item in &mut item.use_ {
+                if use_item.relative {
+                    if !item.line_nspace.name.is_empty() {
+                        if item.line_nspace.relative {
+                            for n in item.line_nspace.name.iter().rev() {
+                                use_item.name.insert(0, n.clone());
+                            }
+                            use_item.name.insert(0, module.name.clone());
+                        } else {
+                            for n in item.line_nspace.name.iter().rev() {
+                                use_item.name.insert(0, n.clone());
+                            }
+                        }
+                    } else {
+                        use_item.name.insert(0, module.name.clone());
                     }
                 }
             }
